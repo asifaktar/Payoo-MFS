f43d93dbdc5ce23c77d2981b60028c2074e40b4d
unknown <asifaktar7767@gmail.com>
2024-09-19 21:21:28 +0530

    added a some text

--- Payoo/features.js
+++ Payoo/features.js
@@ -4,5 +4,19 @@ document.getElementById("btn-cash-out").addEventListener("click", function(){
     document.getElementById("cash-money-form").classList.remove("hidden");
 
     // step no 3 hidden  ta abar add kora cash out a;
-    document.getElementById("add-money-form").classList.remove("hidden");
+    document.getElementById("add-money-form").classList.add("hidden");  
 })
+
+// step no 4 add mney btn ke click kora;
+document.getElementById("btn-add-money").addEventListener('click', function(){
+
+    // step no 5 abar hidden kora hoyeche add money ke;
+    document.getElementById("add-money-form").classList.remove("hidden");
+
+    // step no 6 abar  hidden ke remove kora hoyeche ;
+    document.getElementById('cash-money-form').classList.add("hidden");
+})
+
+// Today my project is complete allhamdulillah;
+
+
